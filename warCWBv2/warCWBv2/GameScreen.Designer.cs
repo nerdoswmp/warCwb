131b5b4e3f63f3a1cfd524b8d4f0c0289f5ae4ab
murilo <nerdoswampo@gmail.com>
2022-06-01 23:34:23 -0300

    Criação de uma nova tela para auxiliar o MapForm

---
+++ warCWBv2/warCWBv2/GameScreen.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace warCWBv2
+{
+    partial class GameScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Location = new System.Drawing.Point(266, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(454, 720);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // GameScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(720, 720);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "GameScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GameScreen";
+            this.Load += new System.EventHandler(this.GameScreen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ warCWBv2/warCWBv2/GameScreen.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static warCWBv2.MapForm;
+
+namespace warCWBv2
+{
+    public partial class GameScreen : Form
+    {
+        public static Team[] teams = CreateTeams();
+        static Player[] players = CreatePlayers();
+        static int current = 0;
+        public GameScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void GameScreen_Load(object sender, EventArgs e)
+        {
+            MapForm mapForm = new MapForm() { TopLevel = false, TopMost = true };
+
+            this.panel1.Controls.Add(mapForm);
+            mapForm.Show();
+
+            
+        }
+
+        public void UpdateLabels()
+        {
+            label1.Text = GetCurrentPlayer().GetTeam().GetColor().Name;
+            switch ((int)GetCurrentPlayer().GetAction())
+            {
+                case 0:
+                    label2.Text = "Place Troops";
+                    break;
+                case 1:
+                    label2.Text = "Attack";
+                    break;
+                case 2:
+                    label2.Text = "Move Troops";
+                    break;
+                case 3:
+                    label2.Text = "Waiting";
+                    break;
+            }
+        }
+        public static Player GetCurrentPlayer()
+        {
+            return players[current];
+        }
+
+        public static void NextPlayer()
+        {
+            if (current >= 3)
+            {
+                current = 0;
+                foreach(var p in players)
+                {
+                    p.ResetAct();
+                }   
+            }
+            else
+            {
+                current++;
+            }
+        }
+        public static Team[] CreateTeams()
+        {
+            Team team1 = new Team(Color.Red);
+            Team team2 = new Team(Color.Yellow);
+            Team team3 = new Team(Color.Green);
+            Team team4 = new Team(Color.Blue);
+
+            return new Team[] { team1, team2, team3, team4 };
+        }
+
+        public static Player[] CreatePlayers()
+        {
+            Player p1 = new Player(teams[0]);
+            Player p2 = new Player(teams[1]);
+            Player p3 = new Player(teams[2]);
+            Player p4 = new Player(teams[3]);
+
+            return new Player[] { p1, p2, p3, p4 };
+        }
+
+
+        public static Team[] GetAllTeams()
+        {
+            return teams;
+        }
+    }
+}
