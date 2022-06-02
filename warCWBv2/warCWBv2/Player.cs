ae0fec4707602172bc81299536fcc2df6bb39532
murilo <nerdoswampo@gmail.com>
2022-06-01 23:32:07 -0300

    Métodos para utilização das ações do player

--- warCWBv2/warCWBv2/Player.cs
+++ warCWBv2/warCWBv2/Player.cs
@@ -10,8 +10,35 @@ namespace warCWBv2
     {
         TurnAction action;
         Team team;
+
+        public Player(Team team)
+        {
+            this.action = 0;
+            this.team = team;
+        }
+
+        public Team GetTeam()
+        {
+            return this.team;
+        }
+
+        public TurnAction GetAction()
+        {
+            return this.action;
+        }
+
+        public void NextAct()
+        {
+            this.action++;
+        }
+
+        public void ResetAct()
+        {
+            this.action = 0;
+        }
     }
 
+    
     public enum TurnAction
     {
         PlaceTroops = 0,
